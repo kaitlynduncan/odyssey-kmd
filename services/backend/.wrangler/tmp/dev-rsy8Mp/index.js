2e416484431e3e447e93973d926cfd15c365ff4e
kaitlynduncan <kaitlynd324@gmail.com>
2026-09-04 12:05:38 -0700

    Back-end bug resolved - docker didn't start correctly

--- services/backend/.wrangler/tmp/dev-rsy8Mp/index.js
+++ services/backend/.wrangler/tmp/dev-rsy8Mp/index.js
@@ -46,14 +46,14 @@ var __publicField = (obj, key, value) => {
 };
 var __template = (cooked, raw2) => __freeze(__defProp(cooked, "raw", { value: __freeze(raw2 || cooked.slice()) }));
 
-// .wrangler/tmp/bundle-1nZy5r/strip-cf-connecting-ip-header.js
+// .wrangler/tmp/bundle-kox48W/strip-cf-connecting-ip-header.js
 function stripCfConnectingIPHeader(input, init) {
   const request = new Request(input, init);
   request.headers.delete("CF-Connecting-IP");
   return request;
 }
 var init_strip_cf_connecting_ip_header = __esm({
-  ".wrangler/tmp/bundle-1nZy5r/strip-cf-connecting-ip-header.js"() {
+  ".wrangler/tmp/bundle-kox48W/strip-cf-connecting-ip-header.js"() {
     __name(stripCfConnectingIPHeader, "stripCfConnectingIPHeader");
     globalThis.fetch = new Proxy(globalThis.fetch, {
       apply(target, thisArg, argArray) {
@@ -8220,14 +8220,14 @@ var require_lib2 = __commonJS({
   }
 });
 
-// .wrangler/tmp/bundle-1nZy5r/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-kox48W/middleware-loader.entry.ts
 init_strip_cf_connecting_ip_header();
 init_modules_watch_stub();
 init_virtual_unenv_global_polyfill_cloudflare_unenv_preset_node_process();
 init_virtual_unenv_global_polyfill_cloudflare_unenv_preset_node_console();
 init_performance2();
 
-// .wrangler/tmp/bundle-1nZy5r/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-kox48W/middleware-insertion-facade.js
 init_strip_cf_connecting_ip_header();
 init_modules_watch_stub();
 init_virtual_unenv_global_polyfill_cloudflare_unenv_preset_node_process();
@@ -24116,7 +24116,7 @@ var jsonError = /* @__PURE__ */ __name(async (request, env2, _ctx, middlewareCtx
 }, "jsonError");
 var middleware_miniflare3_json_error_default = jsonError;
 
-// .wrangler/tmp/bundle-1nZy5r/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-kox48W/middleware-insertion-facade.js
 var __INTERNAL_WRANGLER_MIDDLEWARE__ = [
   middleware_ensure_req_body_drained_default,
   middleware_miniflare3_json_error_default
@@ -24153,7 +24153,7 @@ function __facade_invoke__(request, env2, ctx, dispatch, finalMiddleware) {
 }
 __name(__facade_invoke__, "__facade_invoke__");
 
-// .wrangler/tmp/bundle-1nZy5r/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-kox48W/middleware-loader.entry.ts
 var __Facade_ScheduledController__ = class {
   constructor(scheduledTime, cron, noRetry) {
     this.scheduledTime = scheduledTime;
